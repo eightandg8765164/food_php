0a940d74ccf90ad0f92ee979c36f7d7f520561d9
winnieliu0122 <w8i5n1n2i2@gamil.com>
2016-03-15 21:54:48 +0800

    Added files via upload

--- food_submit_1.php
+++ food_submit_1.php
@@ -1,5 +1,6 @@
 <html>
 	<head>
+		<meta charset="utf-8">
 		<title>訂食吧-註冊會員</title>
 	</head>
 	<body>
@@ -32,15 +33,37 @@
 		<br>
 		<br>
 		<form name="submit" method="post" action="food_submit_2.php">
-			信箱：<input type="text" size=20 name="u_mail"> 姓名：<input type="text" size=20 name="u_name">
-			<br><br>
-			密碼：<input type="text" size=20 name="u_pwd"> 確認密碼：<input type="text" size=20 name="u_pwd">
-			<br><br>
-			手機號碼：<input type="text" size=20 name="u_phone">
-			<input type="submit" value="送出資料">
-		</form>
+		帳號：<input type="text" size=20 name="u_account"> 姓名：<input type="text" size=20 name="u_name">
 		<br>
+		<br>
+		<!--密碼：<input type="text" size=20 name="u_pwd"> 確認密碼：<input type="text" size=20 name="u_pwd">-->
+		密碼<input type=password name="u_pwd" value='' size=10>確認密碼<input type=password name="u_pwdchk" value='' size=10><br>
+		<br>
+		<br>
+		信箱：<input type="text" size=20 name="u_mail">
+		</form>
+
+		
+		<input type="submit" value="送出資料" onClick="return check()" >
 		
 		</center>
+		<script language="JavaScript" type="text/JavaScript">
+		<!--
+		function check(){
+		p1 = document.submit.u_pwd.value;
+		p2 = document.submit.u_pwdchk.value;
+		if ( p1 == p2 ) { 
+			alert("BINGO!!!");
+			location.href = "C:/Users/superuser/desktop/food_submit_2.php";		
+		}
+		else {
+		alert("兩組密碼不一致");
+		location.reload()
+		}
+		}
+		//-->
+		</SCRIPT>
 	</body>
 </html>
+
+
