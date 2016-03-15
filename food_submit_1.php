7d70b272bd76cf976f8ea10742fe61b015c8ce90
jwc911037 <jwc911037@gmail.com>
2016-03-15 22:13:04 +0800

    Added files via upload

--- food_submit_1.php
+++ food_submit_1.php
@@ -1,6 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	
+</body>
+</html>>
 <html>
 	<head>
-		<meta charset="utf-8">
 		<title>訂食吧-註冊會員</title>
 	</head>
 	<body>
@@ -33,37 +44,15 @@
 		<br>
 		<br>
 		<form name="submit" method="post" action="food_submit_2.php">
-		帳號：<input type="text" size=20 name="u_account"> 姓名：<input type="text" size=20 name="u_name">
-		<br>
-		<br>
-		<!--密碼：<input type="text" size=20 name="u_pwd"> 確認密碼：<input type="text" size=20 name="u_pwd">-->
-		密碼<input type=password name="u_pwd" value='' size=10>確認密碼<input type=password name="u_pwdchk" value='' size=10><br>
-		<br>
-		<br>
-		信箱：<input type="text" size=20 name="u_mail">
+			信箱：<input name="u_mail" type="email" required placeholder="thanks@lot.friends" autocomplete="off" maxlength="50">姓名：<input type="text" size=20 name="u_name">
+			<br><br>
+			密碼：<input type="text" size=20 name="u_pwd" required> 確認密碼：<input type="text" size=20 name="u_pwd" required>
+			<br><br>
+			手機號碼：<input name="u_phone" type="tel" placeholder="0912345678" pattern="09[1-8][0-9]([\-|\s]?)[0-9]{3}\1[0-9]{3}" maxlength="20" required>
+			<input type="submit" value="送出資料">
 		</form>
-
-		
-		<input type="submit" value="送出資料" onClick="return check()" >
+		<br>
 		
 		</center>
-		<script language="JavaScript" type="text/JavaScript">
-		<!--
-		function check(){
-		p1 = document.submit.u_pwd.value;
-		p2 = document.submit.u_pwdchk.value;
-		if ( p1 == p2 ) { 
-			alert("BINGO!!!");
-			location.href = "C:/Users/superuser/desktop/food_submit_2.php";		
-		}
-		else {
-		alert("兩組密碼不一致");
-		location.reload()
-		}
-		}
-		//-->
-		</SCRIPT>
 	</body>
 </html>
-
-
