8af209dd2a4db6f32a914dbb8c89ba80cb4acefd
jwc911037 <jwc911037@gmail.com>
2016-03-16 15:04:03 +0800

    Added files via upload

---
+++ food_connect.php
@@ -0,0 +1,38 @@
+<html>
+	<head>
+		<title>訂食吧-登入</title>
+	</head>
+	<body>
+		<?php
+			session_start();
+			//連接資料庫
+			//只要此頁面上有用到連接MySQL就要include它
+			include("food_mysql_connect.inc.php");
+			$id = $_POST['u_account'];
+			$pwd = $_POST['u_pwd'];
+
+			//搜尋資料庫資料
+			$sql = "SELECT * FROM user WHERE u_mail='$id'";
+			$result = mysql_query($sql);
+			$row = @mysql_fetch_row($result);
+			/*
+			echo $row[0];
+			echo $row[1];
+			if($row[1] == $pwd){
+				echo "yayaya";
+			}else{
+				echo "hahaha";
+			}
+			*/
+			//判斷帳號與密碼是否為空白
+			//以及MySQL資料庫裡是否有這個會員
+			if($row[0] == $id && $row[1] == $pwd){
+       		 //將帳號寫入session，方便驗證使用者身份
+       		 	$_SESSION['user_mail'] = $id;
+      		  	echo '登入成功!';
+      		 }else{
+       		 	echo '登入失敗!';
+			}
+		?>
+	</body>
+</html>
